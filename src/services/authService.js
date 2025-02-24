2750922939c7751fb7cfb970de6de08b709aacbc
bachvu-dev <bach@techasia.biz>
2025-02-25 00:24:11 +0700

    feat fix

--- src/services/authService.js
+++ src/services/authService.js
@@ -1,7 +1,7 @@
 // src/services/authService.js
 export const login = async (email, password) => {
   try {
-    const response = await fetch('http://localhost:8080/api/admin/login', {
+    const response = await fetch('https://math-be.onrender.com/api/admin/login', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
